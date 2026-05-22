22562078d280ab4699de8396e1cfc8fba8fc3064
Hyungjun Kim <73098596+Rocketstein@users.noreply.github.com>
2026-05-22 16:15:10 +0900

    Feat: Add Alloc and Free implementation in ParticleDynamicData.cpp Fix: Resolve some type casting warnings

--- KraftonEngine/Source/Engine/Render/Particle/ParticleDynamicData.cpp
+++ KraftonEngine/Source/Engine/Render/Particle/ParticleDynamicData.cpp
@@ -1,2 +1,22 @@
 ﻿#include "ParticleDynamicData.h"
 
+void FParticleDataContainer::Alloc(int32 DataBytes, int32 IndexCount)
+{
+	Free();
+	ParticleDataNumBytes = DataBytes;
+	ParticleIndicesNumShorts = IndexCount;
+	if (DataBytes > 0)
+	{
+		ParticleData = new uint8[DataBytes + IndexCount * sizeof(uint16)];
+		ParticleIndices = reinterpret_cast<uint16*>(ParticleData + DataBytes);
+	}
+}
+
+void FParticleDataContainer::Free()
+{
+	delete[] ParticleData;
+	ParticleData = nullptr;
+	ParticleIndices = nullptr;
+	ParticleDataNumBytes = 0;
+	ParticleIndicesNumShorts = 0;
+}
