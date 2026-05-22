88415003fa5df6a5f3364d7dd94f6da12d183b7c
Hyungjun Kim <73098596+Rocketstein@users.noreply.github.com>
2026-05-22 19:31:12 +0900

    Feat: Add InstanceVB binding in DrawCommandList.cpp

--- KraftonEngine/Source/Engine/Render/Command/DrawCommandList.cpp
+++ KraftonEngine/Source/Engine/Render/Command/DrawCommandList.cpp
@@ -193,6 +193,11 @@ void FDrawCommandList::SubmitCommand(const FDrawCommand& Cmd,
 		{
 			uint32 Offset = 0;
 			Ctx->IASetVertexBuffers(0, 1, &Cmd.Buffer.VB, &Cmd.Buffer.VBStride, &Offset);
+
+			if (Cmd.Buffer.InstancedCount > 0 && Cmd.Buffer.InstanceVB)
+			{
+				Ctx->IASetVertexBuffers(1, 1, &Cmd.Buffer.InstanceVB, &Cmd.Buffer.InstanceVBStride, &Offset);
+			}
 		}
 		if (bForce || Cmd.Buffer.IB != Cache.Buffer.IB)
 		{
