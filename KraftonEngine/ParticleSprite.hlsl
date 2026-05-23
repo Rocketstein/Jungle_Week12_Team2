9431122f1020f1e0a5e3e1640b2de25e5a4c0ec8
Hyungjun Kim <73098596+Rocketstein@users.noreply.github.com>
2026-05-23 10:53:09 +0900

    Minor update in ParticleSprite

--- KraftonEngine/ParticleSprite.hlsl
+++ KraftonEngine/ParticleSprite.hlsl
@@ -1,7 +1,22 @@
 #include "Common/Functions.hlsli"
 #include "Common/VertexLayouts.hlsli"
+#include "Common/SystemSamplers.hlsli"
 
-float4 main() : SV_TARGET
+Texture2D ParticleAtlas : register(t0);
+
+//cbuffer SubUVRegionBuffer : register(b2)
+//{
+//    float4 UVRegion; // xy = offset, zw = size
+//}
+
+PS_Input_Tex VS(VS_Input_ParticleSprite Input)
 {
-	return float4(1.0f, 1.0f, 1.0f, 1.0f);
+    PS_Input Out;
+	
+    return Out;
+}
+
+float4 PS(PS_Input_Tex Input) : SV_Target
+{
+    return float4(1, 1, 1, 1);
 }
