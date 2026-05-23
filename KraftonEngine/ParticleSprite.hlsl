c17a50d7c5f029232f8c6ba8f5bbe9fb7354a8db
Hyungjun Kim <73098596+Rocketstein@users.noreply.github.com>
2026-05-23 11:08:16 +0900

    Feat: Prototype VS implementation

--- KraftonEngine/ParticleSprite.hlsl
+++ KraftonEngine/ParticleSprite.hlsl
@@ -9,10 +9,25 @@ Texture2D ParticleAtlas : register(t0);
 //    float4 UVRegion; // xy = offset, zw = size
 //}
 
+float2 RotateParticleCorner(float2 Corner, float Rotation)
+{
+    float S;
+    float C;
+    sincos(Rotation, S, C);
+    return float2(Corner.x * C - Corner.y * S, Corner.x * S + Corner.y * C);
+}
+
 PS_Input_Tex VS(VS_Input_ParticleSprite Input)
 {
-    PS_Input Out;
-	
+    PS_Input_Tex Out;
+    float2 Corner = Input.uv * 2.0f - 1.0f;
+    Corner = RotateParticleCorner(Corner, Input.rotation);
+    Corner *= Input.size.xy;
+    float4 viewPos = mul(float4(position, 1), View);
+    viewPos.xy += corner;
+    output.position = mul(viewPos, Projection);
+    
+    Out.color = Input.color;
     return Out;
 }
 
