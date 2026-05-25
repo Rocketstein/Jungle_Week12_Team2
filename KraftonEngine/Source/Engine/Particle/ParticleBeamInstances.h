7a8b68d104737c3622b9e8b36c92c734d6242159
Hyungjun Kim <73098596+Rocketstein@users.noreply.github.com>
2026-05-25 13:19:44 +0900

    Feat: Add FBeam2EmitterInstance file

---
+++ KraftonEngine/Source/Engine/Particle/ParticleBeamInstances.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+#include "ParticleEmitterInstances.h"
+
+struct FBeam2EmitterInstance : public FParticleEmitterInstance
+{
+	using FParticleEmitterInstance::FParticleEmitterInstance;
+
+	void Tick(float DeltaTime, bool bSuppressSpawning) override;
+	FDynamicEmitterReplayDataBase* GetReplayData() override;
+};
