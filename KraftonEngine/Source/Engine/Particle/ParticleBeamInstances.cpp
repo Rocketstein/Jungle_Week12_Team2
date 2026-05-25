22eb1d43f81b291f2abe1250680042eacae9ea9f
Hyungjun Kim <73098596+Rocketstein@users.noreply.github.com>
2026-05-25 13:52:18 +0900

    Feat: Update GetReplayData logic

--- KraftonEngine/Source/Engine/Particle/ParticleBeamInstances.cpp
+++ KraftonEngine/Source/Engine/Particle/ParticleBeamInstances.cpp
@@ -1,4 +1,6 @@
 ﻿#include "ParticleBeamInstances.h"
+#include "Particle/ParticleLODLevel.h"
+#include "Component/ParticleSystemComponent.h"
 
 void FBeam2EmitterInstance::Tick(float DeltaTime, bool bSuppressSpawning)
 {
@@ -7,9 +9,38 @@ void FBeam2EmitterInstance::Tick(float DeltaTime, bool bSuppressSpawning)
 
 FDynamicEmitterReplayDataBase* FBeam2EmitterInstance::GetReplayData()
 {
-	if (ActiveParticles <= 0)
+	UParticleModuleTypeDataBeam2* BeamModule = static_cast<UParticleModuleTypeDataBeam2*>(CurrentLODLevel->TypeDataModule);
+	if (!BeamModule) return nullptr;
+
+	FVector Source;
+	FVector Target;
+
+	switch (BeamModule->BeamMethod)
+	{
+	case (PEB2M_Distance):
+	{
+		// TODO: Add "Use Local Space" variant that adds Location to Source
+		Source = BeamModule->SourcePoint;
+		FVector TargetDir = Component ? Component->GetForwardVector() : FVector(1, 0, 0);
+		Target = TargetDir * BeamModule->Distance;
+		break;
+	}
+	case (PEB2M_Target):
+	{
+		Source = BeamModule->SourcePoint;
+		Target = BeamModule->TargetPoint;
+		break;	
+	}
+	case(PEB2M_Branch):
+	{
+		// TODO
+		return nullptr;
+		break;
+	}
+	default:
 	{
 		return nullptr;
+	}
 	}
 
 	FDynamicEmitterReplayDataBase* NewEmitterReplayData = new FDynamicBeamEmitterReplayData();
