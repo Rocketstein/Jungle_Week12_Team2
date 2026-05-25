442c9a1edc03bc98877b84ee8815f715c2277f67
Hyungjun Kim <73098596+Rocketstein@users.noreply.github.com>
2026-05-25 13:26:07 +0900

    Feat: Add GetReplayData implementation for FBeam2EmitterInstance

---
+++ KraftonEngine/Source/Engine/Particle/ParticleBeamInstances.cpp
@@ -0,0 +1,24 @@
+﻿#include "ParticleBeamInstances.h"
+
+void FBeam2EmitterInstance::Tick(float DeltaTime, bool bSuppressSpawning)
+{
+	
+}
+
+FDynamicEmitterReplayDataBase* FBeam2EmitterInstance::GetReplayData()
+{
+	if (ActiveParticles <= 0)
+	{
+		return nullptr;
+	}
+
+	FDynamicEmitterReplayDataBase* NewEmitterReplayData = new FDynamicBeamEmitterReplayData();
+
+	if (!FillReplayData(*NewEmitterReplayData))
+	{
+		delete NewEmitterReplayData;
+		return nullptr;
+	}
+
+	return NewEmitterReplayData;
+}
