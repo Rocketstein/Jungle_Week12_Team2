c4408ce0411cfa04c38df82c28b651d99c5e6b64
Hyungjun Kim <73098596+Rocketstein@users.noreply.github.com>
2026-05-26 23:45:38 +0900

    Baseplate commit

---
+++ KraftonEngine/Source/Engine/Core/UObject/FObjectPtr.cpp
@@ -0,0 +1,1 @@
+﻿#include "FObjectPtr.h"
---
+++ KraftonEngine/Source/Engine/Core/UObject/FObjectPtr.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+#include "Core/ClassTypes.h"
+
+class UObject;
+
+struct FObjectPtr
+{
+public:
+	FObjectPtr() = default;
+	FObjectPtr(const FObjectPtr& Other);
+	FObjectPtr(int Index);
+	FObjectPtr(const UObject* InObject);
+
+	void Reset() {  }
+
+	bool IsValid() const;
+
+	UObject* Get() const;
+
+
+public:
+	bool operator== (const FObjectPtr& Other) const;
+	bool operator!= (const FObjectPtr& Other) const;
+	void operator= (const UObject* Object);
+	FObjectPtr& operator= (const FObjectPtr& Other);
+
+
+protected:
+
+};
